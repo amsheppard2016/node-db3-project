e756cff61884303a988be85202baab5b56a9dc67
Ash W <amsheppard2016@gmail.com>
2020-05-29 00:16:53 -0400

    mvp

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,40 @@
 const db = require("../data/db-config.js");
 
 function find() {
-    return db("Schemes");
+    return db("schemes");
 }
 
-function findById(id) {
-    return db("Schemes").where({ id });
+async function findById(id) {
+    const schemes = await db("schemes").where({ id });
+    return schemes;
 }
+
+async function findSteps(id) {
+    const steps = await db("schemes as s")
+        .join("steps as st", "s.id", "st.scheme_id")
+        .select("s.id", "s.scheme_name", "st.step_number", "st.instructions")
+        .where("s.id", id);
+    return steps;
+}
+
+async function add(scheme) {
+    const [newSchemeId] = await db("schemes").insert(scheme);
+    return findById(newSchemeId);
+}
+
+function update(changes, id) {
+    return db("schemes").where({ id }).update(changes);
+}
+
+function remove(id) {
+    return db("schemes").where({ id }).del();
+}
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove,
+};
